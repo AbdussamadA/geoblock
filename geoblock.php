271e30ae7c5aaaa4f46fd5fbe2af949dec12b869
abdussamad <abdussamad@hilo.localhost>
2016-03-01 17:18:27 +0500

    Version v0.3 - Added license file

--- geoblock.php
+++ geoblock.php
@@ -4,7 +4,25 @@ namespace Abd\WordPress\Plugins;
 Plugin Name: GeoIP content blocker
 Description: Block content based on the user's geographical location. Enter country codes separated by commas like this: [geoblock type='blacklist' countries='pk,in']. Type can be blacklist or whitelist
 Author: Abdussamad
-Version: 0.2
+Version: 0.3
+License: GPLv2
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License version 2
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+License Details: http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 */
 
 class geoblocker {
